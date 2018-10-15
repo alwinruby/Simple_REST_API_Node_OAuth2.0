102c5c44e4a47dd425e2794f542684423702de04
asolanky <alwinsolanky@gmail.com>
2018-10-15 12:14:33 +0100

    Added database.js but not working

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,14 +1,18 @@
-const express = require('express')
+//@@ -2,10 + 2,14 @@
+const express = require('express') //added @@s
 const bodyParser = require('body-parser')
 const { promisify } = require('util')
+const initializeDatabase = require('./database')//added
 
 const app = express()
 app.use(bodyParser.json())
 
 const startServer = async () => {
+  await initializeDatabase(app) //added
+
   const port = process.env.SERVER_PORT || 3000
   await promisify(app.listen).bind(app)(port)
   console.log(`Listening on port ${port}`)
 }
 
-startServer()
+startServer()s
